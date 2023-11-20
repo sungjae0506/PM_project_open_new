b364a2037af29e1c9f25673f7a8d7cb009e1091c
Yumin22224 <yumin224@snu.ac.kr>
2023-11-20 23:25:46 +0900

    fmod 라이브러리 추가

---
+++ PM_project_open_new/src/game/game_manager.cpp
@@ -0,0 +1,48 @@
+#include "game_manager.h"
+
+void GameManager::clear() {
+
+}
+void GameManager::addPlayer(Entity* player) {
+
+}
+
+void GameManager::stateChange() {
+
+}
+
+void GameManager::clearMapPath(void) {
+
+}
+void GameManager::addMapPath(string path) {
+
+}
+void GameManager::loadMap(void) {
+
+}
+
+void GameManager::move() {
+
+}
+
+void GameManager::drawMap() {
+
+}
+void GameManager::drawMapChange() {
+
+}
+
+void GameManager::keyboardEvent() {
+
+}
+void GameManager::idleEvent() {
+
+}
+
+
+////////////////////////////////////////////////////////////////////////
+
+
+void GameManager::draw() {
+
+}
