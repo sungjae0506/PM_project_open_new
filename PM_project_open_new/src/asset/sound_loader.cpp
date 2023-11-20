ed5c5c2d13480172cce2a2a969969e14778dd86f
Yumin22224 <yumin224@snu.ac.kr>
2023-11-20 23:28:15 +0900

    fmod 코드 (아직 수정X)

---
+++ PM_project_open_new/src/asset/sound_loader.cpp
@@ -0,0 +1,51 @@
+#include "sound_loader.h"
+
+FMOD_SYSTEM* Sound::g_sound_system;
+
+Sound::Sound(const char* p, bool l) {
+
+    if (loop)
+        FMOD_System_CreateSound(g_sound_system, path, FMOD_LOOP_NORMAL, 0, &m_sound);
+    else
+        FMOD_System_CreateSound(g_sound_system, path, FMOD_LOOP_OFF, 0, &m_sound);
+
+    loop = l;
+    path = p;
+
+    m_channel = nullptr;
+    m_volume = SOUND_DEFAULT;
+}
+
+void Sound::initsound(bool run) {
+
+    result = FMOD::System_Create(&system);
+    result = system->getVersion(&version);
+
+    result = system->init(32, FMOD_INIT_NORMAL, extradriverdata);
+    if (loop)
+        result = system->createSound(path, FMOD_LOOP_NORMAL, 0, &soundfile);
+    else
+        result = system->createSound(path, FMOD_LOOP_OFF, 0, &soundfile);
+    if (run)
+        result = system->playSound(soundfile, 0, false, &channel);
+}
+
+void Sound::updatesound() {
+    result = system->update();
+}
+
+void Sound::playsound() {
+    result = system->playSound(soundfile, 0, false, &channel);
+}
+
+void Sound::pausesound() {
+    result = channel->setPaused(true);
+}
+
+void Sound::resumesound() {
+    result = channel->setPaused(false);
+}
+
+void Sound::releasesound() {
+    system->release();
+}
--- PM_project_open_new/src/asset/sound_loader.h
+++ PM_project_open_new/src/asset/sound_loader.h
@@ -1,1 +1,40 @@
 #pragma once
+#include "inc/fmod.h"
+#include "inc/fmod.hpp"
+#include <iostream>
+#include <conio.h>
+
+#define SOUND_MAX 1.0f
+#define SOUND_MIN 0.0f
+#define SOUND_DEFAULT 0.5f
+#define SOUND_WEIGHT 0.1f
+
+
+class Sound {
+public:
+	Sound(const char* p, bool l);
+	FMOD::System* system;
+	FMOD::Sound* soundfile;
+	FMOD::Channel* channel;
+
+	void initsound(bool run);
+	void updatesound();
+	void playsound();
+	void pausesound();
+	void resumesound();
+	void releasesound();
+
+	const char* path;
+	bool loop;
+
+	unsigned int version;
+	void* extradriverdata;
+
+	static FMOD_SYSTEM* g_sound_system;
+	FMOD_SOUND* m_sound;
+	FMOD_CHANNEL* m_channel;
+
+	float m_volume;
+	FMOD_BOOL m_bool;
+	FMOD_RESULT result;
+};
